57eb812ca84e90ce82f86e7bdb7f1d6883cfb5f9
Leif Mørstad <leif@moerstad.no>
2024-03-29 13:50:37 +0100

    added julia transformations

---
+++ src/main/java/edu/ntnu/stud/model/math/JuliaTransformation.java
@@ -0,0 +1,38 @@
+package edu.ntnu.stud.model.math;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents a julia transformation.
+ *
+ * @author Leif Mørstad
+ * @version 1.0
+ */
+public class JuliaTransformation {
+  private final ComplexNumber c;
+
+  /**
+   * Creates a new instance with the given complex number.
+   *
+   * @param c the complex number to use in the transformation
+   */
+  public JuliaTransformation(ComplexNumber c) {
+    this.c = c;
+  }
+
+  /**
+   * Transforms the given complex number using the complex number of this transformation, and
+   * randomly negates the result.
+   *
+   * @param z the complex number to transform
+   * @return the transformed complex number
+   */
+  public ComplexNumber transform(@NotNull ComplexNumber z) {
+    ComplexNumber transformed = z.subtract(c).sqrt();
+    if (Math.random() < 0.5) {
+      return transformed;
+    }
+    return transformed.multiply(-1);
+  }
+
+}
